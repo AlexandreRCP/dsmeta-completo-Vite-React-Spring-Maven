710136acf56173aaaaed4b81a38ba6ce8653f3fd
AlexandreRCP <alex.rcp@hotmail.com>
2022-11-16 12:34:28 -0300

    Security config

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -11,7 +11,6 @@ function App() {
                 <div className="dsmeta-container">
                   <Header />
                   <SalesCard />
-                  <NotificationButton />
                 </div>
         </section>
       </main>             
