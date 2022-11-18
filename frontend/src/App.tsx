9e476a1a9a71907260f19df2930f49746f98b3c8
AlexandreRCP <alex.rcp@hotmail.com>
2022-11-18 18:46:18 -0300

    Toast

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,5 @@
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import Header from './components/Header';
 import SalesCard from './components/SalesCard';
 import NotificationButton from './components/NotificationButton';
@@ -6,6 +8,7 @@ import './index.css';
 function App() {
   return (
     <>
+      <ToastContainer />
       <main>
             <section id="sales">
                 <div className="dsmeta-container">
