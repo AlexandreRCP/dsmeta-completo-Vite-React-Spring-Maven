4843806a593c61abfa09360b248a6ed3e44e3e23
AlexandreRCP <alex.rcp@hotmail.com>
2022-11-18 18:29:36 -0300

    Send notification

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,9 +1,21 @@
+import axios from 'axios';
 import vector from '../../assets/img/vector.svg';
+import { BASE_URL } from '../../utils/request';
 import './styles.css';
 
-function NotificationButton() {
+type Props = {
+  saleId: number;
+}
+
+function handleClick(id : number){
+  axios(`${BASE_URL}/sales/${id}/notification`).then(response => {
+    console.log("Sucesso!");
+  });
+}
+
+function NotificationButton( {saleId} : Props) {
     return (
-      <div className="dsmeta-red-btn">
+      <div className="dsmeta-red-btn" onClick={() => handleClick(saleId)}>
         <img src={vector} alt="Notificar" />
       </div>
     )
