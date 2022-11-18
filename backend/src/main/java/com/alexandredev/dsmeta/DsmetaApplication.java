54e4dc3cb7a33f848a83248247db111a16b62412
AlexandreRCP <alex.rcp@hotmail.com>
2022-11-18 20:30:09 -0300

    update

--- backend/src/main/java/com/alexandredev/dsmeta/DsmetaApplication.java
+++ backend/src/main/java/com/alexandredev/dsmeta/DsmetaApplication.java
@@ -3,6 +3,7 @@ package com.alexandredev.dsmeta;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//Inicio da aplicação
 @SpringBootApplication
 public class DsmetaApplication {
 
